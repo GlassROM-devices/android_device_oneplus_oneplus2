dd6e2b732ecc4df60ade462cf90b730081b7d05f
anupritaisno1 <www.anuprita804@gmail.com>
2018-05-13 20:24:58 +0100

    op2: add initial treble rules treble means trouble
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- device.mk
+++ device.mk
@@ -117,7 +117,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
     camera.device@3.3-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
