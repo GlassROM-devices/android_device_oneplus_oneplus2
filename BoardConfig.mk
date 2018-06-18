279c82f1b11048143bafec42348bb41a06760a15
anupritaisno1 <www.anuprita804@gmail.com>
2018-06-18 20:58:25 +0100

    Revert "oneplus2: Enable QSEE keymaster flag"
    This reverts commit b3b649e3e9cfe84b5fea4e7184c5bb4ec56c7a3a.
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,9 +176,6 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
 
-# Keymaster
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
